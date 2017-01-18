08cdace576db74c8c2010f069e4f5f6aef9e320d
Kid <302890512@qq.com>
2017-01-18 18:57:30 +0800

    Add UIData.cs

---
+++ UI/UIData.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GUIFrame
+{
+    
+    public class UIData {
+        public bool isStartWindow       = false;
+        public UIType uiType            = UIType.Normal;
+        public UIShowMode showMode      = UIShowMode.DoNothing;
+        public UIColliderMode collider  = UIColliderMode.None;
+
+    }
+
+    public class BackUISequenceData
+    {
+       // TODO 
+    }
+
+    public class ShowUIData
+    {
+        //  Reset窗口
+        public bool forceResetUI            = false;
+        // Clear导航信息
+        public bool forceClearBackSeqData   = false;
+        // Object 数据
+        public object data;
+    }
+
+    public delegate bool BoolDelegate();
+}
